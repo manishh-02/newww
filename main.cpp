bc987c6c06b89b1466a73821fe8d11a91e447b72
MANISH KUMAR MISHRA <mishra.manish4763@gmail.com>
2026-08-28 21:53:34 +0530

    Fix buffer overflow in array and string handling
    Fixed buffer overflow issues with array and string copy.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 int main() {
     int* arr = new int[5];
@@ -7,6 +8,12 @@ int main() {
         arr[i] = i;
     }
 
+    char src[] = "This string is too long";
+    char dest[5];
+
+    strcpy(dest, src);
+
     delete[] arr;
+
     return 0;
 }
